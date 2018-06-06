9f7da47ced4b3951f35f7cceaad82a561f483453
TonyWhiteb <tonybai2016@gmail.com>
2018-06-06 12:25:43 -0500

    CusUserModel_test1

--- LoginTEST/myweb/LoginSession/MyUser.py
+++ LoginTEST/myweb/LoginSession/MyUser.py
@@ -1,6 +1,41 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
 from django.db import models
 
-class User(AbstractUser):
-    email = models.EmailField(unique = True)
-    password = models.TextField(max_length = 500)
+
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,password = None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )    
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,password):
+        user = self.create_user(
+            email,
+            password = password
+        )    
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(verbos_name = 'email_add', maxlength = 255, unique = True)
+    password = models.TextField( max_length = 255)
+    is_admin = models.BooleanField(default = False)
+    object = MyUserManager()
+    
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self,perm,obj = None):
+        return True
+
+    def has_module_perms(self,app_label):
+        return True
+    
--- LoginTEST/myweb/LoginSession/models.py
+++ LoginTEST/myweb/LoginSession/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class MyUser(AbstractUser):
+    # email = models.EmailField(verbose_name = 'email_add', maxlength = 255, unique = True)
+    # password = models.TextField(max_length = 500)
+    # USERNAME_FIELD = 'email'
+    
+    # def __str__(self):
+    #     return self.email
+
+    # def    
+    pass
