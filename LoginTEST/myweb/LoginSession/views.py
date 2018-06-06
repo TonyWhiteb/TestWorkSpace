3f4eb3ae594f6c3de0e09b95a309da4bdb9cfa97
TonyWhiteb <tonybai2016@gmail.com>
2018-06-06 15:22:46 -0500

    CreateIndexTestView

--- LoginTEST/myweb/LoginSession/views.py
+++ LoginTEST/myweb/LoginSession/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,HttpResponse
-
+from LoginSession.models import MyUser
 # Create your views here.
 def baseview(request):
     return render(request,'base.html')
+
+
+def indextest(request):
+     pass
