0494b554d95522f66bd35038beaf2fccbf90cc21
TonyWhiteb <tonybai2016@gmail.com>
2018-06-08 15:37:26 -0500

    AddVenvsEnv

---
+++ .venvs/TestWorkSpace-_N5xTqk7/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tonyt\desktop\project\testworkspace\.venvs\testworkspace-_n5xtqk7\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
